e1f57d20af5931497619f2f216d5fd6f299584aa
nessmamohamed <42851861+nessmamohamed@users.noreply.github.com>
2020-06-24 21:10:53 +0200

    Add files via upload

---
+++ client/build/precache-manifest.eeef25146a93f5a0b4fe99f519d9c183.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d967829ff436e1f87a8ef44522d8cb16",
+    "url": "/market/index.html"
+  },
+  {
+    "revision": "a10f7ffb672a51aa7da4",
+    "url": "/market/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "e455d2b273b48fd571c3",
+    "url": "/market/static/css/main.985e8e19.chunk.css"
+  },
+  {
+    "revision": "a10f7ffb672a51aa7da4",
+    "url": "/market/static/js/2.6ee31ea7.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/market/static/js/2.6ee31ea7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e455d2b273b48fd571c3",
+    "url": "/market/static/js/main.be05dd87.chunk.js"
+  },
+  {
+    "revision": "b71a9c1f35a499a0ef62",
+    "url": "/market/static/js/runtime-main.454223c0.js"
+  },
+  {
+    "revision": "dd24df745c840dfa52afca087b4954dc",
+    "url": "/market/static/media/product1.dd24df74.jpg"
+  },
+  {
+    "revision": "8e805071b893dcaf544bb2174ce6783b",
+    "url": "/market/static/media/product10.8e805071.jpg"
+  },
+  {
+    "revision": "9df21201b3ea2a9c85388bca70a7188e",
+    "url": "/market/static/media/product11.9df21201.jpg"
+  },
+  {
+    "revision": "559e811a426b0ffe8f71795cc2fcb066",
+    "url": "/market/static/media/product12.559e811a.jpg"
+  },
+  {
+    "revision": "d58e97e066045aca8559b77418d1000f",
+    "url": "/market/static/media/product2.d58e97e0.jpg"
+  },
+  {
+    "revision": "e5a602bb39fc8fbe6881d34f5958c4d5",
+    "url": "/market/static/media/product3.e5a602bb.jpg"
+  },
+  {
+    "revision": "798bf8bdc72e451b33b4253545638dc4",
+    "url": "/market/static/media/product4.798bf8bd.jpg"
+  },
+  {
+    "revision": "6a6d06a8a1a6f4d5a1d3bc6f6dc3adf3",
+    "url": "/market/static/media/product5.6a6d06a8.png"
+  },
+  {
+    "revision": "c2bb17e3366def40538446629836e174",
+    "url": "/market/static/media/product6.c2bb17e3.jpg"
+  },
+  {
+    "revision": "4f879dea86bdae0788154b5c0428d404",
+    "url": "/market/static/media/product7.4f879dea.jpg"
+  },
+  {
+    "revision": "8ffe2011f6dcada1ca8542cc7399f764",
+    "url": "/market/static/media/product8.8ffe2011.jpg"
+  },
+  {
+    "revision": "2476c09233da8a6de93d3ca1ae43566e",
+    "url": "/market/static/media/product9.2476c092.jpg"
+  }
+]);
